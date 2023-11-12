b8d988a209603d269425f9dcf55abac0f92c6513
JunnyJun <chbe5082@naver.com>
2023-11-12 22:51:46 +0900

    jdbcClient

--- src/main/kotlin/com/example/playground/http/jdbcClient/SampleJdbcClient.kt
+++ src/main/kotlin/com/example/playground/http/jdbcClient/SampleJdbcClient.kt
@@ -1,5 +1,6 @@
 package com.example.playground.http.jdbcClient
 
+import org.springframework.jdbc.core.namedparam.SimplePropertySqlParameterSource
 import org.springframework.jdbc.core.simple.JdbcClient
 import org.springframework.stereotype.Component
 
@@ -12,6 +13,24 @@ class SampleJdbcClient(
             .sql("SELECT * FROM SAMPLE_TABLE",)
             .query(Response::class.java)
             .list()
+
+    fun insert(): Int = jdbcClient
+            .sql("INSERT INTO SAMPLE_TABLE (id, name) VALUES (?1, ?2)",)
+            .paramSource(
+                Response(
+                    id = 1,
+                    name = "Super Mario Odyssey",
+                )
+            ).update()
+
+    fun update(): Int = jdbcClient
+            .sql("UPDATE SAMPLE_TABLE SET name = ?1 WHERE id = ?2")
+            .paramSource(
+                Response(
+                    id = 1,
+                    name = "Super Mario Odyssey",
+                )
+            ).update()
 
     data class Response(
         val id: Int,
