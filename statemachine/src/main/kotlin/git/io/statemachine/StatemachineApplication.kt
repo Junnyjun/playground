9c8aaf05f7f270e074d654ace3d964bb5db9b768
junny <chbe5082@naver.com>
2024-05-07 22:32:56 +0900

    statemachine init

---
+++ statemachine/src/main/kotlin/git/io/statemachine/StatemachineApplication.kt
@@ -0,0 +1,11 @@
+package git.io.statemachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatemachineApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatemachineApplication>(*args)
+}
