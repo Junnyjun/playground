566122a402f09dd3f3b7af6046fd3d30e6c6bef3
junny <chbe5082@naver.com>
2024-05-09 19:42:46 +0900

    setup

--- statemachine/src/main/kotlin/git/io/statemachine/state/StateUsecase.kt
+++ statemachine/src/main/kotlin/git/io/statemachine/state/StateUsecase.kt
@@ -12,16 +12,13 @@ fun interface StateSelector {
 
     @Service
     class StateSelectorUsecase(
-        private val stateMachine: StateMachine<Status, Action>
+        private val stateMachine: StateMachine<String,String>
     ):  StateSelector{
         override fun invoke() {
-            val message1 = Mono.just(MessageBuilder.withPayload(Action.A1).build())
-            stateMachine.sendEvent(message1)
-            stateMachine.sendEvent(message1)
-            val message2 = Mono.just(MessageBuilder.withPayload(Action.A2).build())
-            stateMachine.sendEvent(message2)
-            val message3 = Mono.just(MessageBuilder.withPayload(Action.A3).build())
-            stateMachine.sendEvent(message3)
+            stateMachine.start()
+            stateMachine.sendEvent(Mono.just(MessageBuilder.withPayload("E1").build())).subscribe()
+            stateMachine.sendEvent(Mono.just(MessageBuilder.withPayload("E2").build())).subscribe()
+            stateMachine.sendEvent(Mono.just(MessageBuilder.withPayload("EE").build())).subscribe()
         }
 
     }
