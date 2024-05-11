55a15ba1324943838492db4b3449a0db49b9807c
junny <chbe5082@naver.com>
2024-05-11 22:56:55 +0900

    init

---
+++ src/test/kotlin/git/io/discovery/DiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
