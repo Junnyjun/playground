5a7bf6a100df8c819c98952a773ebfce07e557fc
Junnyjun <chbe5082@naver.com>
2024-09-18 16:16:35 +0900

    init

---
+++ kakfa/src/main/kotlin/git/io/kakfa/KakfaApplication.kt
@@ -0,0 +1,11 @@
+package git.io.kakfa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakfaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KakfaApplication>(*args)
+}
---
+++ kakfa/src/test/kotlin/git/io/kakfa/KakfaApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.kakfa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KakfaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
