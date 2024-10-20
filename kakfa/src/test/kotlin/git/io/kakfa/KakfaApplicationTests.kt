a051575086f19790fbf5211f14d6e2833cefe5c1
Junnyjun <chbe5082@naver.com>
2024-10-20 23:36:03 +0900

    feat (producer) : conroutine sample

--- kakfa/src/test/kotlin/git/io/kakfa/KakfaApplicationTests.kt
+++ kakfa/src/test/kotlin/git/io/kakfa/KakfaApplicationTests.kt
@@ -1,7 +1,11 @@
 package git.io.kakfa
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
 import java.io.FileInputStream
 import java.io.InputStream
 import java.util.concurrent.atomic.AtomicLong
@@ -11,6 +15,8 @@ class KakfaApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
 		val stream = FileInputStream("src/main/resources/application.properties")
 		AtomicLong()
 	}
