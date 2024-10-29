fd97bc709cda99e68731912d724535d3aa565441
Junnyjun <chbe5082@naver.com>
2024-10-30 00:55:56 +0900

    hibernate setup

--- rsocket/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ rsocket/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,18 @@
 package com.example.demo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication: CommandLineRunner{
+	fun main(args: Array<String>) {
+		runApplication<DemoApplication>(*args)
+	}
 
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	override fun run(vararg args: String?) {
+
+	}
+
+
 }
