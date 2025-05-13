0a0edd81a1b840fae6fb9bc1619f18f8ca78229e
Junnyjun <chbe5082@naver.com>
2025-05-13 19:28:30 +0900

    set up

--- integration/src/main/kotlin/io/junnyland/integration/database/DBintegration.kt
+++ integration/src/main/kotlin/io/junnyland/integration/database/DBintegration.kt
@@ -1,41 +1,38 @@
 package io.junnyland.integration.database
 
+import io.junnyland.integration.jpa.Message
+import jakarta.persistence.EntityManagerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.integration.config.EnableIntegration
 import org.springframework.integration.dsl.IntegrationFlow
-import org.springframework.integration.jdbc.JdbcPollingChannelAdapter
-import javax.sql.DataSource
-import org.springframework.integration.jdbc.dsl.Jdb
 import org.springframework.integration.jpa.core.JpaExecutor
 import org.springframework.integration.jpa.dsl.Jpa
 
 @Configuration
 @EnableIntegration
 class DBintegration(
-	private val dataSource: DataSource
+	private val entitiManagerFactory: EntityManagerFactory
 ) {
-
-
 	@Bean
 	fun dbIntegrationFlow(): IntegrationFlow {
 		return IntegrationFlow.from(
-			Jpa.inboundAdapter()
-
-		}.get(
-
-		)
-
+			Jpa.inboundAdapter(entitiManagerFactory)
+		) {
+		}.get()
 	}
 
-@Bean
-fun jpaExecutor(): JpaExecutor {
-	val jpaExecutor = JpaExecutor()
-	jpaExecutor.setEntityClass(Message::class.java)
-	jpaExecutor.setJpaQuery("from Message where processed = false")
-	jpaExecutor.setDeleteAfterPoll(false)
-	jpaExecutor.setDeleteInBatch(false)
-	return jpaExecutor
-}
+	private fun processMessage(message: Any) {
+		// 여기서 실제 메시지 처리 로직을 구현합니다.
+		println("Processing message: $message")
+	}
 
+	@Bean
+	fun jpaExecutor(entityManagerFactory: EntityManagerFactory): JpaExecutor {
+		val jpaExecutor = JpaExecutor(entityManagerFactory)
+		jpaExecutor.setEntityClass(Message::class.java)
+		jpaExecutor.setJpaQuery("from Message where processed = false")
+		jpaExecutor.setDeleteAfterPoll(false)
+		return jpaExecutor
+	}
 }
