fec0f40f6dab92e4f0abc10ccb87ebfa6dd0f676
Junnyjun <chbe5082@naver.com>
2025-11-09 23:22:56 +0900

    enhance Producer class with configuration options and message sending functionality

---
+++ sup/src/main/kotlin/kafka/Consumer.kt
@@ -0,0 +1,36 @@
+package io.junnyland.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import java.util.Properties
+
+class Consumer(
+	private val topic: String,
+	private val bootstrap: String = "localhost:9092",
+) {
+	private val consumer: KafkaConsumer<String, String> = configure()
+
+	fun configure(): KafkaConsumer<String, String> = Properties().also {
+		it[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrap
+		it[ConsumerConfig.GROUP_ID_CONFIG] = "TEST_GROUP"
+		it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+		it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+	}.let {
+		KafkaConsumer(it)
+	}
+
+	fun receive() {
+		consumer.subscribe(listOf(topic))
+		consumer.poll(Long.MAX_VALUE).forEach {
+			println("Received: ${it.value()}")
+		}
+	}
+}
+
+fun main() {
+	Consumer(
+		"test",
+	).receive(
+	)
+}
